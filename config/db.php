974ebf52b94cec92dcf6d5a71d417f25e7c1c65c
Francesco Terenzani <f.terenzani@gmail.com>
2022-09-02 14:40:13 +0200

    Fix db config

--- config/db.php
+++ config/db.php
@@ -2,5 +2,5 @@
 /**
  * Return an instance of the database object
  */
-$db = new \pp\Db($_ENV['DB_DSN'], $_ENV['DB_USERNAME'], @$_ENV['DB_PASSWORD']);
+$db = new \pp\Db($_ENV['DB_DSN'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DRIVER_OPTIONS']);
 return $db;
